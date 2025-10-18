8be2e0855ff1269d0152045727f73ed1eff289f2
RASBI <74168235+RASBI-CARDNAME@users.noreply.github.com>
2025-10-18 18:15:27 +0900

    Update and rename main.c to Core/Src/main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,18 +1,7 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
+  MADE BY RASBI
   ******************************************************************************
   */
 /* USER CODE END Header */
@@ -414,3 +403,4 @@ void assert_failed(uint8_t *file, uint32_t line)
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
+
